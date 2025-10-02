30aee4854551cb2495727dc57bd3b05a7ddfc655
Megan Loe <meganhelenaloe@gmail.com>
2025-10-02 10:20:26 +0200

    the first steps

---
+++ Lab1_Megan_Loe_DE25.py
@@ -0,0 +1,49 @@
+#task 1 instructions for the lab
+
+#read the text file called dna_raw.txt
+#each sequene is composed of two lines of data: the first line beginning with > sign is the sequence ID, while the following line is the actual sequence
+#the actual sequence is not case-sensitive, which means that lower and upper case letters are treated the same
+
+
+#for each sequence, create a dictionary to count the number of each DNA letter in that sequence
+#for each sequence, graph the frequency of DNA letters for each sequence
+
+
+#all of the above is the instructions for this lab, I copied into here so I don't have to constantly switch windows and lose track of where I was
+
+
+#this area is for the menus
+#this is the opening menu
+def DNA_menu():
+    print("1. dna txt file")
+    print()
+    print("2. the more complicated dna txt file")
+    print()
+    print("3. end")
+    print()
+    print()
+
+#menu for the first txt file
+def DNA_menu_raw():
+    print("1")
+    print()
+    print("2")
+    print()
+    print("3")
+    print()
+    print("4")
+    print()
+    print()
+
+#menu for the second txt file
+def DNA_menu_complicated():
+    print("1")
+    print()
+    print("2")
+    print()
+    print("3")
+    print()
+    print("4")
+    print()
+    print()
+
