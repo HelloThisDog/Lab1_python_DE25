49a8c892ffa2551c0df20ece2d4ce017116a462f
Megan Loe <meganhelenaloe@gmail.com>
2025-10-08 11:45:13 +0200

    sequence lines are now specified

--- Lab1_Megan_Loe_DE25.py
+++ Lab1_Megan_Loe_DE25.py
@@ -14,6 +14,7 @@
 #this is here just in case, I need to use it
 import re
 
+import linecache
 
 
 
@@ -55,17 +56,22 @@ def DNA_menu_complicated():
 
 #dna counting, its counts! partially. . .
 def dna_count(DNA):
+    #the dictionary for the letters that need to be found
     dna_letters = {
         "A" : 0, 
         "C" : 0, 
         "G" : 0, 
         "T" : 0}
+    
+    #to count the letters
     for letters in DNA:
         dna_letters ["A"] += 1
         dna_letters ["C"] += 1
         dna_letters ["G"] += 1
         dna_letters ["T"] += 1
         print(dna_letters)
+    
+    #need to do an if statement in this function
 
 #the loop
 while True:
@@ -74,12 +80,7 @@ while True:
      DNA_menu()
      dna_menu = input("which file would you like to check? ")
      if dna_menu == "1":
-        dna_raw = open("dna_raw.txt")
-        print(dna_raw)
-        dna_count(dna_raw)
-        dna_raw.close()
-        
-        break
+        DNA_menu_raw()
         
     
      elif dna_menu == "2":
@@ -89,7 +90,36 @@ while True:
      elif dna_menu == "3":
          print("ok ending program")
          break
-     #breaking the whole loop for now until the dictonary is up and running
+
+        #the menu to select sequence for the first file
+     if dna_menu == "1":
+         dna_seq_1 = input("which sequence would you like to run? ") #selects the sequence to read
+
+        #opens the file
+         dna_raw = open("dna_raw.txt")
+
+
+         if dna_seq_1 == "1":
+             seq1_raw = linecache.getline("dna_raw.txt", 2)
+             print(seq1_raw)
+             break
+         
+         elif dna_seq_1 == "2":
+             seq2_raw = linecache.getline("dna_raw.txt", 4)
+             print(seq2_raw)
+             break
+         
+         elif dna_seq_1 =="3":
+             seq3_raw = linecache.getline("dna_raw.txt", 6)
+             print(seq3_raw)
+             break
+         
+         elif dna_seq_1 =="4":
+             seq4_raw = linecache.getline("dna_raw.txt", 8)
+             print(seq4_raw)
+             break
+
+
 
      
 
